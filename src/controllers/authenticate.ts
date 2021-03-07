d39a352246fa9909af040843b7864c4e80081f54
Erfan <erfanasbarixx@gmail.com>
2021-03-07 05:30:38 +0330

    Added status codes and role check function

--- src/controllers/authenticate.ts
+++ src/controllers/authenticate.ts
@@ -1,24 +1,37 @@
 import { Request, Response, NextFunction } from "express";
 import passport from "passport";
 import bcrypt from "bcrypt";
+import { Model } from "sequelize";
 import sequelize from "../database/sequelize";
 
 export async function registerRoute(req: Request, res: Response) {
-	if (await sequelize.models.user.findOne({ where: { username: req.body.username } })) return res.json({ message: "username already exists" });
-	const hash = await bcrypt.hash(req.body.password, 10);
-	const user = await sequelize.models.user.create({
-		username: req.body.username,
-		password: hash,
-		email: req.body.email,
-		salt_key: "empty",
-	});
-	res.json({ message: "success" });
+	try {
+		if (await sequelize.models.user.findOne({ where: { username: req.body.username } })) return res.json({ message: "username already exists" });
+		const hash = await bcrypt.hash(req.body.password, 10);
+		const user = await sequelize.models.user.create({
+			username: req.body.username,
+			password: hash,
+			email: req.body.email,
+			salt_key: "empty",
+		});
+		res.json({ message: "success" });
+	} catch (error) {
+		res.status(500).json({
+			errors: [{ message: "server error" }],
+		});
+		throw error;
+	}
 }
 
 export async function loginRoute(req: Request, res: Response, next: NextFunction) {
 	passport.authenticate("local", (error, user, info) => {
-		if (error) throw error;
-		if (!user) res.json(info);
+		if (error) {
+			res.status(500).json({
+				errors: [{ message: "server error" }],
+			});
+			throw error;
+		}
+		if (!user) res.status(401).json(info);
 		else {
 			req.login(user, (err) => {
 				if (err) throw err;
@@ -33,16 +46,36 @@ export function logoutRoute(req: Request, res: Response) {
 	res.json({ message: "success" });
 }
 
+export function userRoute(req: Request, res: Response) {
+	const user = req.user as Model;
+	res.json({
+		id: user.get("id"),
+		username: user.get("username"),
+		email: user.get("email"),
+		role: user.get("role"),
+	});
+}
+
 export function checkIsAuthenticated(req: Request, res: Response, next: NextFunction) {
 	if (req.isAuthenticated()) return next();
-	res.json({
-		message: "no user logged in.",
+	res.status(400).json({
+		errors: [{ message: "no user logged in" }],
 	});
 }
 
 export function checkIsNotAuthenticated(req: Request, res: Response, next: NextFunction) {
 	if (!req.isAuthenticated()) return next();
-	res.json({
-		message: "already logged in",
+	res.status(400).json({
+		errors: [{ message: "already logged in" }],
 	});
+}
+
+export function checkUserRole(role: number) {
+	return async (req: Request, res: Response, next: NextFunction) => {
+		const user = req.user as Model;
+		if ((user.get("role") as number) >= role) return next();
+		res.status(403).json({
+			errors: [{ message: "insufficient role" }],
+		});
+	};
 }
--- src/routes/api/auth.ts
+++ src/routes/api/auth.ts
@@ -1,24 +1,15 @@
 import express from "express";
-import { Model } from "sequelize";
 import { body } from "express-validator";
 import { validateSequential } from "../../include/validator";
 
 // Controllers
-import { registerRoute, loginRoute, logoutRoute, checkIsAuthenticated, checkIsNotAuthenticated } from "../../controllers/authenticate";
+import { registerRoute, loginRoute, logoutRoute, userRoute, checkIsAuthenticated, checkIsNotAuthenticated } from "../../controllers/authenticate";
 
 let router = express.Router();
 
-router.post("/register", checkIsNotAuthenticated, validateSequential([body("username").isString().isLength({ min: 5 }), body("email").isEmail(), body("password").isLength({ min: 6 })]), registerRoute);
-router.post("/login", checkIsNotAuthenticated, validateSequential([body("username").isString().isLength({ min: 5 }), body("password").isLength({ min: 6 })]), loginRoute);
-router.post("/logout", checkIsAuthenticated, logoutRoute);
-router.get("/user", checkIsAuthenticated, (req, res) => {
-	const user = req.user as Model;
-	res.json({
-		id: user.get("id"),
-		username: user.get("username"),
-		email: user.get("email"),
-		role: user.get("role"),
-	});
-});
+router.post("/register", checkIsNotAuthenticated, validateSequential([body("username").isString().isLength({ min: 5, max: 32 }), body("email").isEmail().normalizeEmail(), body("password").isLength({ min: 6 })]), registerRoute);
+router.post("/login", checkIsNotAuthenticated, validateSequential([body("username").isString().isLength({ min: 5, max: 32 }), body("password").isLength({ min: 6 })]), loginRoute);
+router.delete("/logout", checkIsAuthenticated, logoutRoute);
+router.get("/user", checkIsAuthenticated, userRoute);
 
 export default router;
