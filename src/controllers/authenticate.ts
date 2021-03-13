96fe0d0188e235c58a2efa6c54b4e20abb83129b
Erfan <erfanasbarixx@gmail.com>
2021-03-13 21:43:27 +0330

    Fixed undefined req.user

--- src/controllers/authenticate.ts
+++ src/controllers/authenticate.ts
@@ -3,6 +3,12 @@ import passport from "passport";
 import bcrypt from "bcrypt";
 import prisma from "../prisma/client";
 
+export function RequestHasUser(req: Request): asserts req is Request & Express.AuthenticatedRequest {
+	if (!("user" in req)) {
+		throw new Error("Request object without user found unexpectedly");
+	}
+}
+
 export async function registerRoute(req: Request, res: Response) {
 	try {
 		if (await prisma.users.findUnique({ where: { username: req.body.username } })) return res.status(401).json({ errors: [{ message: "This username already exists." }] });
@@ -49,17 +55,20 @@ export function logoutRoute(req: Request, res: Response) {
 }
 
 export function userRoute(req: Request, res: Response) {
-	const user = req.user;
+	RequestHasUser(req);
 	res.json({
-		id: user?.id,
-		username: user?.username,
-		email: user?.email,
-		role: user?.role,
+		id: req.user.id,
+		username: req.user.username,
+		email: req.user.email,
+		role: req.user.role,
 	});
 }
 
 export function checkIsAuthenticated(req: Request, res: Response, next: NextFunction) {
-	if (req.isAuthenticated()) return next();
+	if (req.isAuthenticated()) {
+		if (!req.user) req.logOut();
+		else return next();
+	}
 	res.status(400).json({
 		errors: [{ message: "no user logged in" }],
 	});
@@ -74,8 +83,8 @@ export function checkIsNotAuthenticated(req: Request, res: Response, next: NextF
 
 export function checkUserRole(role: number) {
 	return async (req: Request, res: Response, next: NextFunction) => {
-		const user = req.user;
-		if ((user?.role as number) >= role) return next();
+		RequestHasUser(req);
+		if (req.user.role >= role) return next();
 		res.status(403).json({
 			errors: [{ message: "insufficient role" }],
 		});
