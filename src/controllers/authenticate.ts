4effb0e18385dbc3b66594c0df4d229891845e57
Erfan <erfanasbarixx@gmail.com>
2021-03-14 18:37:36 +0330

    Moved route handlers out of controllers folder

--- src/controllers/authenticate.ts
+++ src/controllers/authenticate.ts
@@ -1,67 +1,9 @@
 import { Request, Response, NextFunction } from "express";
-import passport from "passport";
-import bcrypt from "bcrypt";
-import prisma from "../prisma/client";
 
 export function RequestHasUser(req: Request): asserts req is Request & Express.AuthenticatedRequest {
 	if (!("user" in req)) {
 		throw new Error("Request object without user found unexpectedly");
 	}
-}
-
-export async function registerRoute(req: Request, res: Response) {
-	try {
-		if (await prisma.users.findUnique({ where: { username: req.body.username } })) return res.status(401).json({ errors: [{ message: "This username already exists." }] });
-		const hash = await bcrypt.hash(req.body.password, 10);
-		const user = await prisma.users.create({
-			data: {
-				username: req.body.username,
-				password: hash,
-				email: req.body.email,
-			},
-		});
-		req.logIn(user, () => {
-			res.json({ message: "success" });
-		});
-	} catch (error) {
-		res.status(500).json({
-			errors: [{ message: "server error" }],
-		});
-		throw error;
-	}
-}
-
-export async function loginRoute(req: Request, res: Response, next: NextFunction) {
-	passport.authenticate("local", (error, user, info) => {
-		if (error) {
-			res.status(500).json({
-				errors: [{ message: "server error" }],
-			});
-			throw error;
-		}
-		if (!user) res.status(401).json({ errors: [info] });
-		else {
-			req.login(user, (err) => {
-				if (err) throw err;
-				res.json({ message: "success" });
-			});
-		}
-	})(req, res, next);
-}
-
-export function logoutRoute(req: Request, res: Response) {
-	req.logOut();
-	res.json({ message: "success" });
-}
-
-export function userRoute(req: Request, res: Response) {
-	RequestHasUser(req);
-	res.json({
-		id: req.user.id,
-		username: req.user.username,
-		email: req.user.email,
-		role: req.user.role,
-	});
 }
 
 export function checkIsAuthenticated(req: Request, res: Response, next: NextFunction) {
--- src/routes/api/auth.ts
+++ src/routes/api/auth.ts
@@ -1,15 +1,68 @@
 import express from "express";
+import passport from "passport";
+import bcrypt from "bcrypt";
 import { body } from "express-validator";
 import { validateSequential } from "../../include/validator";
+import prisma from "../../prisma/client";
 
 // Controllers
-import { registerRoute, loginRoute, logoutRoute, userRoute, checkIsAuthenticated, checkIsNotAuthenticated } from "../../controllers/authenticate";
+import { RequestHasUser, checkIsAuthenticated, checkIsNotAuthenticated } from "../../controllers/authenticate";
 
 let router = express.Router();
 
-router.post("/register", checkIsNotAuthenticated, validateSequential([body("username").isString().isLength({ min: 3, max: 32 }), body("email").isEmail().normalizeEmail(), body("password").isLength({ min: 6 })]), registerRoute);
-router.post("/login", checkIsNotAuthenticated, validateSequential([body("username").isString().isLength({ min: 3, max: 32 }), body("password").isLength({ min: 6 })]), loginRoute);
-router.delete("/logout", checkIsAuthenticated, logoutRoute);
-router.get("/user", checkIsAuthenticated, userRoute);
+router.post("/register", checkIsNotAuthenticated, validateSequential([body("username").isString().isLength({ min: 3, max: 32 }), body("email").isEmail().normalizeEmail(), body("password").isLength({ min: 6 })]), async (req, res) => {
+	try {
+		if (await prisma.users.findUnique({ where: { username: req.body.username } })) return res.status(401).json({ errors: [{ message: "This username already exists." }] });
+		const hash = await bcrypt.hash(req.body.password, 10);
+		const user = await prisma.users.create({
+			data: {
+				username: req.body.username,
+				password: hash,
+				email: req.body.email,
+			},
+		});
+		req.logIn(user, () => {
+			res.json({ message: "success" });
+		});
+	} catch (error) {
+		res.status(500).json({
+			errors: [{ message: "server error" }],
+		});
+		throw error;
+	}
+});
+
+router.post("/login", checkIsNotAuthenticated, validateSequential([body("username").isString().isLength({ min: 3, max: 32 }), body("password").isLength({ min: 6 })]), (req, res, next) => {
+	passport.authenticate("local", (error, user, info) => {
+		if (error) {
+			res.status(500).json({
+				errors: [{ message: "server error" }],
+			});
+			throw error;
+		}
+		if (!user) res.status(401).json({ errors: [info] });
+		else {
+			req.login(user, (err) => {
+				if (err) throw err;
+				res.json({ message: "success" });
+			});
+		}
+	})(req, res, next);
+});
+
+router.delete("/logout", checkIsAuthenticated, (req, res) => {
+	req.logOut();
+	res.json({ message: "success" });
+});
+
+router.get("/user", checkIsAuthenticated, (req, res) => {
+	RequestHasUser(req);
+	res.json({
+		id: req.user.id,
+		username: req.user.username,
+		email: req.user.email,
+		role: req.user.role,
+	});
+});
 
 export default router;
