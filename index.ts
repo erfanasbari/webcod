7b0d7f70b67685c5bd73d45e1d1d746ee00bf328
Erfan <erfanasbarixx@gmail.com>
2021-04-06 20:32:16 +0430

    🐛 Fixed statics not loading on subroutes

--- index.ts
+++ index.ts
@@ -1,9 +1,11 @@
 import express from "express";
 import path from "path";
+import fs from "fs";
 import cors from "cors";
 import helmet from "helmet";
 import passport from "passport";
 import session from "express-session";
+import cheerio from "cheerio";
 import config from "@config/configuration";
 import prisma from "@db/client";
 import configurePassport from "@config/passport";
@@ -41,11 +43,14 @@ const startServer = () => {
 	app.use("/api", apiRoute);
 
 	// ====== Serve React client app ====== //
+	// Adding <base> to index.html
+	const $IndexHtml = cheerio.load(fs.readFileSync(path.join(__dirname + "/client/build/index.html"), "utf8"));
+	$IndexHtml("head title").after(`<base href="${config.address.url}/" />`);
+	const indexHtml = $IndexHtml.html();
+
+	app.get(["/", "/index.html"], (req, res) => res.send(indexHtml)); // Send modified index instead of index.html
 	app.use(express.static(path.join(__dirname, "/client/build"))); // Serve React static files
-	// Render app index.html file to client on any route that doesn't found; So react-router can handle that.
-	app.get("*", (req, res) => {
-		res.sendFile(path.join(__dirname + "/client/build/index.html"));
-	});
+	app.get("*", (req, res) => res.send(indexHtml)); // Send modified index on 404 so react-router can handle that
 
 	// ====== Listning for requests ====== //
 	app.listen(config.address.port, () => {
