3aaa22c2cf50ea4aaa564f569df13d2ed2ad1f49
ThePokerFaCcCe <thepokerfa3e@gmail.com>
2021-06-17 17:59:28 +0430

    :bug: .env.development now replaces .env keys

--- src/config/dotenv.js
+++ src/config/dotenv.js
@@ -1,3 +1,4 @@
+const fs = require("fs");
 const dotenv = require("dotenv");
 const dotenvExpand = require("dotenv-expand");
 const path = require("path");
@@ -7,11 +8,18 @@ const checkEnvResult = (configOutput) => {
 	if (expandOutput.error) throw expandOutput.error;
 };
 
+const readEnvFileAndOverwrite = (path) => {
+	const envConfig = dotenv.parse(fs.readFileSync(path));
+	for (const k in envConfig) {
+		process.env[k] = envConfig[k];
+	}
+};
+
 checkEnvResult(dotenv.config({ path: path.resolve(__dirname, "../../.env") }));
 
 switch (process.env.NODE_ENV) {
 	case "development": {
-		checkEnvResult(dotenv.config({ path: path.resolve(__dirname, "../../.env.development") }));
+		readEnvFileAndOverwrite(path.resolve(__dirname, "../../.env.development"));
 		break;
 	}
 }
