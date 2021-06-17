1ddb627b47a2332b3dde9e6adc9ebf33ffbe7947
ThePokerFaCcCe <thepokerfa3e@gmail.com>
2021-06-17 18:09:58 +0430

    :art: prettier

--- src/config/dotenv.js
+++ src/config/dotenv.js
@@ -4,24 +4,24 @@ const dotenvExpand = require("dotenv-expand");
 const path = require("path");
 
 const checkEnvResult = (configOutput) => {
-	const expandOutput = dotenvExpand(configOutput);
-	if (expandOutput.error) throw expandOutput.error;
+  const expandOutput = dotenvExpand(configOutput);
+  if (expandOutput.error) throw expandOutput.error;
 };
 
 const readEnvFileAndOverwrite = (path) => {
-	const envConfig = dotenv.parse(fs.readFileSync(path));
-	for (const k in envConfig) {
-		process.env[k] = envConfig[k];
-	}
+  const envConfig = dotenv.parse(fs.readFileSync(path));
+  for (const k in envConfig) {
+    process.env[k] = envConfig[k];
+  }
 };
 
 checkEnvResult(dotenv.config({ path: path.resolve(__dirname, "../../.env") }));
 
 switch (process.env.NODE_ENV) {
-	case "development": {
-		readEnvFileAndOverwrite(path.resolve(__dirname, "../../.env.development"));
-		break;
-	}
+  case "development": {
+    readEnvFileAndOverwrite(path.resolve(__dirname, "../../.env.development"));
+    break;
+  }
 }
 
 checkEnvResult(dotenv.config({ path: path.resolve(__dirname, "../prisma/prisma.env") }));
